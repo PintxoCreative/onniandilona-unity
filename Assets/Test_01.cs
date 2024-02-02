a7d9767c44d2dd4baa9e58c0d799c91ed193c3d9
RaduMery <tiina.radmer@hotmail.com>
2024-02-02 13:48:08 +0200

    Set up and test

---
+++ Assets/Test_01.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_01 : MonoBehaviour
+{
+    public float ammount;
+    public object animal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
